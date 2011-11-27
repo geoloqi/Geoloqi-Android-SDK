d60d423a04a2d02bdcee6b67b71501aa5c3bbdd5
Tristan Waddington <tristan.waddington@gmail.com>
2011-11-26 17:08:14 -0800

    Stub out AndroidManifest and some basic functionality for the test projects main activity.

--- src/com/geoloqi/android/sample/LauncherActivity.java
+++ src/com/geoloqi/android/sample/LauncherActivity.java
@@ -2,14 +2,26 @@ package com.geoloqi.android.sample;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-public class LauncherActivity extends Activity
-{
-    /** Called when the activity is first created. */
+public class LauncherActivity extends Activity implements View.OnClickListener {
+    public static final String TAG = "LauncherActivity";
+    
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        findViewById(R.id.home_button_1).setOnClickListener(this);
+    }
+    
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+        case R.id.home_button_1:
+            Log.d(TAG, "Clicked!");
+            break;
+        }
     }
 }
