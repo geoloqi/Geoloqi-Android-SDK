449bce406bbd8b09a4fffa521cec9f4283e53085
Tristan Waddington <tristan.waddington@gmail.com>
2011-12-17 13:32:06 -0800

    Stub out a basic tracker implementation with a new anonymous user account.

--- src/com/geoloqi/android/sample/ui/LauncherActivity.java
+++ src/com/geoloqi/android/sample/ui/LauncherActivity.java
@@ -1,17 +1,18 @@
 package com.geoloqi.android.sample.ui;
 
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetSocketAddress;
-
 import com.geoloqi.android.sample.R;
+import com.geoloqi.android.sdk.LQException;
+import com.geoloqi.android.sdk.LQRequest;
+import com.geoloqi.android.sdk.LQSession;
+import com.geoloqi.android.sdk.LQSession.OnCreateAnonymousUserAccountListener;
+import com.geoloqi.android.sdk.LQTracker;
+import com.geoloqi.android.sdk.LQTracker.LQTrackerProfile;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 
-public class LauncherActivity extends Activity implements View.OnClickListener {
+public class LauncherActivity extends Activity {
     public static final String TAG = "LauncherActivity";
     public static final String LOCALHOST = "10.0.2.2";
     public static final int LOCALHOST_UDP_PORT = 43333;
@@ -21,43 +22,21 @@ public class LauncherActivity extends Activity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        findViewById(R.id.home_button_1).setOnClickListener(this);
-    }
-    
-    @Override
-    public void onClick(View v) {
-        switch(v.getId()) {
-        case R.id.home_button_1:
-            Log.d(TAG, "Clicked!");
-            try {
-                // This debug code sends a byte to the local UDP server.
-                byte[] message = new byte [80];
-                DatagramPacket packet = new DatagramPacket(message, 0, message.length);
-                DatagramSocket socket = new DatagramSocket();
-                socket.connect(new InetSocketAddress(LOCALHOST, LOCALHOST_UDP_PORT));
-                // TODO: The production server will return a 32-bit unix timestamp if the request was received.
-                // TODO: The key here are asynchronous requests:
-                //         - Send a packet and exit.
-                //         - Another thread should receive responses.
-                socket.send(packet);
-                
-                // Get response synchronously
-                DatagramPacket receivePacket = new DatagramPacket(new byte[32], 32);
-                socket.receive(receivePacket);
-                byte[] response = receivePacket.getData();
-                Log.d(TAG, "Response length: "+response.length);
-                long value = 0;
-                for (int i = 0; i < response.length; i++) {
-                   value += (response[i] & 0xff) << (8 * i);
+        final LQTracker tracker = LQTracker.getInstance();
+        
+        if (tracker.getSession() == null) {
+            LQSession.createAnonymousUserAccount(new OnCreateAnonymousUserAccountListener() {
+                @Override
+                public void onCreateAnonymousUserAccount(LQSession session,
+                        LQRequest request, LQException e) {
+                    if (session != null) {
+                        tracker.setSession(session);
+                        tracker.setProfile(LQTrackerProfile.LOGGING);
+                    } else {
+                        Log.e(TAG, e.getMessage());
+                    }
                 }
-                Log.d(TAG, "Response: "+value);
-                
-                // Close connection
-                socket.close();
-            } catch (Exception e) {
-                Log.e(TAG, "RuntimeException!", e);
-            }
-            break;
+            });
         }
     }
 }
