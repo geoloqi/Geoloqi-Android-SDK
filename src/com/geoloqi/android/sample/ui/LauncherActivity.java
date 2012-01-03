02ce091a18dd9f6f8b745675ef308386484180e7
Tristan Waddington <tristan.waddington@gmail.com>
2012-01-02 18:24:04 -0800

    Bump build target and minSdkVersion from 4 to 5. Add declarations for LQService and LQSettingsActivity to AndroidManifest.xml. Add basic options menu to LauncherActivity.

--- src/com/geoloqi/android/sample/ui/LauncherActivity.java
+++ src/com/geoloqi/android/sample/ui/LauncherActivity.java
@@ -1,33 +1,55 @@
 package com.geoloqi.android.sample.ui;
 
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+
 import com.geoloqi.android.sample.R;
 import com.geoloqi.android.sdk.LQException;
-import com.geoloqi.android.sdk.LQRequest;
 import com.geoloqi.android.sdk.LQSession;
 import com.geoloqi.android.sdk.LQSession.OnCreateAnonymousUserAccountListener;
 import com.geoloqi.android.sdk.LQTracker;
 import com.geoloqi.android.sdk.LQTracker.LQTrackerProfile;
+import com.geoloqi.android.sdk.service.LQService;
+import com.geoloqi.android.sdk.service.LQService.LQBinder;
+import com.geoloqi.android.sdk.ui.LQSettingsActivity;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.util.Log;
-
-public class LauncherActivity extends Activity {
+public class LauncherActivity extends Activity implements View.OnClickListener {
     public static final String TAG = "LauncherActivity";
+    public static final String LOCALHOST = "10.0.2.2";
+    public static final int LOCALHOST_UDP_PORT = 43333;
+    
+    private LQService mService;
+    private boolean mBound;
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
+        // TODO: When do we start the service for the first time? LQSession or LQTracker?
+        
+        /*
         final LQTracker tracker = LQTracker.getInstance();
         
         if (tracker.getSession() == null) {
             LQSession.createAnonymousUserAccount(new OnCreateAnonymousUserAccountListener() {
                 @Override
-                public void onCreateAnonymousUserAccount(LQSession session,
-                        LQRequest request, LQException e) {
+                public void onCreateAnonymousUserAccount(LQSession session, LQException e) {
                     if (session != null) {
+                        // Create a new anonymous session for the tracker.
+                        // TODO: Where are we going to pull these key and secret values from?
+                        //session.setAPIKey("key", "secret");
+                        
                         tracker.setSession(session);
                         tracker.setProfile(LQTrackerProfile.LOGGING);
                     } else {
@@ -36,5 +58,100 @@ public class LauncherActivity extends Activity {
                 }
             });
         }
+        */
+        
+        // Bind to LQService so we can call methods on it.
+        Intent intent = new Intent(this, LQService.class);
+        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+        
+        //findViewById(R.id.home_button_1).setOnClickListener(this);
     }
+    
+    @Override
+    public void onPause() {
+        super.onPause();
+        
+        // Unbind from LQService
+        if (mBound) {
+            unbindService(mConnection);
+            mBound = false;
+        }
+    }
+    
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+        case R.id.home_button_1:
+            Log.d(TAG, "Clicked!");
+            /*
+            try {
+                // This debug code sends a byte to the local UDP server.
+                byte[] message = new byte [80];
+                DatagramPacket packet = new DatagramPacket(message, 0, message.length);
+                DatagramSocket socket = new DatagramSocket();
+                socket.connect(new InetSocketAddress(LOCALHOST, LOCALHOST_UDP_PORT));
+                // TODO: The production server will return a 32-bit unix timestamp if the request was received.
+                // TODO: The key here are asynchronous requests:
+                //         - Send a packet and exit.
+                //         - Another thread should receive responses.
+                socket.send(packet);
+                
+                // Get response synchronously
+                DatagramPacket receivePacket = new DatagramPacket(new byte[32], 32);
+                socket.receive(receivePacket);
+                byte[] response = receivePacket.getData();
+                Log.d(TAG, "Response length: "+response.length);
+                long value = 0;
+                for (int i = 0; i < response.length; i++) {
+                   value += (response[i] & 0xff) << (8 * i);
+                }
+                Log.d(TAG, "Response: "+value);
+                
+                // Close connection
+                socket.close();
+            } catch (Exception e) {
+                Log.e(TAG, "RuntimeException!", e);
+            }
+            */
+            break;
+        }
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+        case R.id.settings:
+            startActivity(new Intent(this, LQSettingsActivity.class));
+            return true;
+        }
+        return false;
+    }
+    
+    /** Defines callbacks for service binding, passed to bindService() */
+    private ServiceConnection mConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            try {
+                // We've bound to LocalService, cast the IBinder and get LocalService instance.
+                LQBinder binder = (LQBinder) service;
+                mService = binder.getService();
+                mBound = true;
+            } catch (ClassCastException e) {
+                // Pass
+            }
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mBound = false;
+        }
+    };
 }
