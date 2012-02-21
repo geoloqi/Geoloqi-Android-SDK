c2a1d54534a7573d1090f3c43231884c99b92c8e
Tristan Waddington <tristan.waddington@gmail.com>
2012-02-20 23:32:24 -0800

    Add ConstantsTemplate example file.

---
+++ src/com/geoloqi/android/sample/ConstantsTemplate.java
@@ -0,0 +1,11 @@
+package com.geoloqi.android.sample;
+
+/**
+ * Rename this interface to 'Constants.java' when setting up
+ * the sample app. Update the {@link #LQ_SDK_ID} and {@link #LQ_SDK_SECRET}
+ * values to your Geoloqi application ID and SECRET.
+ */
+public interface ConstantsTemplate {
+    public static final String LQ_SDK_ID = "...";
+    public static final String LQ_SDK_SECRET = "...";
+}
